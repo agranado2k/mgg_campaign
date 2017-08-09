b4094e272193f524f20fa38f2f51f2f983ef7251
Arthur Granado <agranado@pluga.co>
2017-08-09 19:15:28 -0300

    UseCases::MigrateCampaignData -> create pre structure

--- app/use_cases/migrate_campaign_data.rb
+++ app/use_cases/migrate_campaign_data.rb
@@ -1,9 +1,38 @@
 module UseCases
   class MigrateCampaignData
     DATA_PATTERN = 'VOTE\s+\d+\s+Campaign\:[\d\w\_]+\s+Validity\:\w+\s+Choice\:\w+\s+CONN:[\w\d]+\s+MSISDN\:\d+\s+GUID\:[\w\d\-]+\s+Shortcode\:\d+'
+    CAMPAIGN_PATTERN = 'Campaign\:([\d\w\_]+)'
+    VALIDITY_PATTERN = 'Validity\:(\w+)'
+    CHOICE_PATTERN = 'Choice\:(\w+)'
+    CONN_PATTERN = 'CONN:([\w\d]+)'
+    MSISDN_PATTERN = 'MSISDN\:(\d+)'
+    GUID_PATTERN = 'GUID\:([\w\d\-]+)'
+    SHORT_CODE_PATTERN = 'Shortcode\:(\d+)'
+
+    def create_pre_structure(lines)
+      lines.reduce({}) do |r, line|
+        hash = convert_line_to_hash(line)
+        r[hash[:Campaign]] = [] if r[hash[:Campaign]].nil?
+        r[hash[:Campaign]].push(hash)
+        r
+      end
+    end
 
     def valid?(data)
       Regexp.new(DATA_PATTERN).match(data)
+    end
+
+    def convert_line_to_hash(line)
+      campaign = Regexp.new(CAMPAIGN_PATTERN).match(line)[1]
+      validity = Regexp.new(VALIDITY_PATTERN).match(line)[1]
+      choice = Regexp.new(CHOICE_PATTERN).match(line)[1]
+      conn = Regexp.new(CONN_PATTERN).match(line)[1]
+      msisdn = Regexp.new(MSISDN_PATTERN).match(line)[1]
+      guid = Regexp.new(GUID_PATTERN).match(line)[1]
+      short_code = Regexp.new(SHORT_CODE_PATTERN).match(line)[1]
+
+      {Campaign: campaign, Validity: validity, Choice: choice, CONN: conn, 
+       MSISDN: msisdn, GUID: guid, Shortcode: short_code.to_i}
     end
   end
 end
--- test/use_cases/migrate_campaign_data_test.rb
+++ test/use_cases/migrate_campaign_data_test.rb
@@ -4,7 +4,40 @@ require_relative "../../app/use_cases/migrate_campaign_data"
 
 module MigrateCampaignDataTest
 
-  class ValidateTest < Minitest::Test
+  class ProcessDataTest < Minitest::Test
+    def setup
+      @migrate_campaign = UseCases::MigrateCampaignData.new
+    end
+
+    def test_convert_data_line_to_hash
+      line = "VOTE 1168042591 Campaign:ssss_uk_01B Validity:during Choice:Jane CONN:MIG01TU MSISDN:00777779489999 GUID:190E96A8-89CA-41F8-9611-91F3490ACCA4 Shortcode:63334"
+      hash = {Campaign: "ssss_uk_01B", Validity: "during", Choice: "Jane", CONN: "MIG01TU", MSISDN: "00777779489999", GUID: "190E96A8-89CA-41F8-9611-91F3490ACCA4", Shortcode: 63334}
+
+      assert_equal hash, @migrate_campaign.convert_line_to_hash(line)
+    end
+
+    def test_create_pre_structure_with_data
+      lines = [
+        "VOTE 1168042591 Campaign:ssss_uk_01B Validity:during Choice:Jane CONN:MIG01TU MSISDN:00777779489999 GUID:190E96A8-89CA-41F8-9611-91F3490ACCA4 Shortcode:63334",
+        "VOTE 1168042606 Campaign:ssss_uk_01B Validity:during Choice:Tupele CONN:MIG01TU MSISDN:00777773559999 GUID:A375C7FE-B748-480B-B8D4-5CE86A79762B Shortcode:63334",
+        "VOTE 1168042652 Campaign:Emmerdale Validity:during Choice:GRAYSON CONN:MIG01OU MSISDN:00777771739999 GUID:36E30841-0B9A-47FF-9A13-AA97A27A2E18 Shortcode:63339"
+      ]
+
+      pre_structure = {
+        "ssss_uk_01B" => [
+          {Campaign: "ssss_uk_01B", Validity: "during", Choice: "Jane", CONN: "MIG01TU", MSISDN: "00777779489999", GUID: "190E96A8-89CA-41F8-9611-91F3490ACCA4", Shortcode: 63334},
+          {Campaign: "ssss_uk_01B", Validity: "during", Choice: "Tupele", CONN: "MIG01TU", MSISDN: "00777773559999", GUID: "A375C7FE-B748-480B-B8D4-5CE86A79762B", Shortcode: 63334}
+        ],
+        "Emmerdale" => [
+          {Campaign: "Emmerdale", Validity: "during", Choice: "GRAYSON", CONN: "MIG01OU", MSISDN: "00777771739999", GUID: "36E30841-0B9A-47FF-9A13-AA97A27A2E18", Shortcode: 63339}
+        ]
+      }
+
+      assert_equal pre_structure, @migrate_campaign.create_pre_structure(lines)
+    end
+  end
+
+  class ValidateDataTest < Minitest::Test
     def setup
       @migrate_campaign = UseCases::MigrateCampaignData.new
     end
