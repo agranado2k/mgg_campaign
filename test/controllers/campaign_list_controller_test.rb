3fbb584f96d24ea48125319c9da97cec2b920a7d
Arthur Granado <agranado@pluga.co>
2017-08-17 13:18:47 -0300

    Remove some not need folders

--- test/controllers/campaign_list_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CampaignControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get campaign_index_url
-    assert_response :success
-  end
-
-end
