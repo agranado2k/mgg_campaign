579d899b80751c5a31c2a551f8c4d57f44d8d0f0
Arthur Granado <agranado@pluga.co>
2017-08-17 13:35:41 -0300

    Include Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,8 +8,7 @@
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
--- app/controllers/campaign_controller.rb
+++ app/controllers/campaign_controller.rb
@@ -6,6 +6,6 @@ class CampaignController < ApplicationController
   def detail
     name = params[:id]
     campaign = UseCases::CampaignByName.new.find_by(name)
-    @details = UseCases::CampaignDetails.new.detail(campaign).sort{|e| e[:valid_votes]}.reverse
+    @details = UseCases::CampaignDetails.new.detail(campaign).sort_by{|e| e[:valid_votes]}.reverse
   end
 end
